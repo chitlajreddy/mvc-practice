375b6eb7032ce4ac5794be09dfb8ee4f08629669
chitlajreddy <js9179@CBR-DES-DEL0971.htcindia.com>
2018-02-26 09:59:15 +0530

    adding code to git

---
+++ practice/webapp1/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webapp1.Models;
+
+namespace webapp1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random(string isbn)
+        {
+            ECOMM_S3Entities r = new ECOMM_S3Entities();
+
+            List<Movies> minfo = new List<Movies>();
+            var data = r.sp_getproducts().ToList();
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].ISBN_13.ToString().ToLower() == isbn)
+                {
+                    Movies uobj = new Movies();
+                    uobj.ISBN_13 = data[i].ISBN_13.ToString();
+                    uobj.Title = data[i].Title.ToString();
+                    minfo.Add(uobj);
+                }
+            }
+            //ViewBag.userdetails = minfo;
+            return View(minfo.AsEnumerable());
+            //var movie = new Movies { category = "Telugu", Name = "AGNI!!" };
+            //return View(movie);
+        }
+
+        public ActionResult MovieList()
+        {
+            return Content("test movie -- lan-> telugu ....");
+        }
+    }
+}
---
+++ practice/webapp1/Models/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapp1.Models
+{
+    public class Movies
+    {
+        public string ISBN_13 { get; set; }
+        public string Title { get; set; }
+
+        public string category{ get; set; }
+
+        public List<Movies> movieinfo { get; set; }
+    }
+}
